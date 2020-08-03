5bc3f04fb0c2d8832a658a2bfc8478056443121d
willow <wsmoyer313@gmail.com>
2020-08-03 13:34:32 -0500

    wip

--- {{cookiecutter.project_slug}}/backend/config/settings.py
+++ {{cookiecutter.project_slug}}/backend/config/settings.py
@@ -94,10 +94,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env.str('POSTGRES_DB'),
-        'USER': env.str('POSTGRES_USER'),
-        'PASSWORD': env.str('POSTGRES_PASSWORD'),
-        'HOST': 'postgres',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
         'PORT': 5432,
     },
 }
